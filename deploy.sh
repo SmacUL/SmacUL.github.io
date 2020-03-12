bb3d8fdf9eca1ed83413498ef9c3c064ef6d2d0d
SmacUL <1164253125@qq.com>
2020-03-12 20:43:03 +0800

    code

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add *
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:SmacUL/SmacUL.github.io.git master
+# git push -f git@github.com:SmacUL/SmacUL.github.io.git master
+git push -f git@gitee.com:smacul/SmacUL.git master
 
 cd ../../../
 git add *
